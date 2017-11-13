bb8c2a376b7cca3b3b9e2230abbdea50f08334ca
Alex Ng <alexander.ng@oracle.com>
2017-11-13 11:26:46 -0800

    Add glusterfs sample configuration
    This adds a sample configuration for creating a glusterfs volume that's
    replicated across 3 ADs. This sample is taken from gilson.melo@oracle.com
    with the following changes:
    
    - Add networking resources required by the glusterfs servers and clients
    - Rename/Introduce some variables to parameterize the sample more consistently
    with the other samples
    - Added a README file to explain the configuration

---
+++ docs/examples/clusters/glusterfs/variables.tf
@@ -0,0 +1,37 @@
+// These settings can be populated here or read from your env-vars settings
+
+// Settings for authentication
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "region" {}
+variable "private_key_path" {}
+variable "private_key_password" {}
+
+variable "compartment_ocid" {}
+
+// The SSH public key for connecting to the compute instances
+variable "ssh_public_key" {}
+
+// The name DNS label to use for the VCN
+variable "DnsLabel" {}
+
+variable "ServerInstanceShape" {
+  default = "BM.DenseIO1.36"
+}
+
+variable "ClientInstanceShape" {
+  default = "VM.Standard1.2"
+}
+
+variable "ServerInstanceImage" {
+  default = "CentOS-7-2017.10.19-0"
+}
+
+variable "ClientInstanceImage" {
+  default = "Oracle-Linux-7.4-2017.10.25-0"
+}
+
+variable "BootStrapFile" {
+  default = "./userdata/bootstrap"
+}
